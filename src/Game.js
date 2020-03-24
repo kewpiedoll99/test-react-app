ee4607cb9a03c3294d4ea569fb0d60a2316baf04
Barclay Dunn <bdunn@renttherunway.com>
2020-03-24 12:05:57 -0400

    updated versions of minimist and acorn

--- src/Game.js
+++ src/Game.js
@@ -4,13 +4,17 @@ import { Board } from './Board';
 export class Game extends React.Component {
     render() {
         return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+            <div>
+                <h1>Hello world!</h1>
+                <div className="game">
+                    <br/>
+                    <div className="game-board">
+                        <Board />
+                    </div>
+                    <div className="game-info">
+                        <div>{/* status */}</div>
+                        <ol>{/* TODO */}</ol>
+                    </div>
                 </div>
             </div>
         );
